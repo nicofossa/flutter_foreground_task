43ac8992e5c33aca66ee388f347bbfcad87ee11b
Nicola Fossati <nicola.fossati@ibtsystems.it>
2024-05-23 17:09:00 +0200

    fix(broadcast-receiver): changing from not exported to exported in order to receive notification button press intents
    Signed-off-by: Nicola Fossati <nicola.fossati@ibtsystems.it>

--- android/src/main/kotlin/com/pravera/flutter_foreground_task/service/ForegroundService.kt
+++ android/src/main/kotlin/com/pravera/flutter_foreground_task/service/ForegroundService.kt
@@ -175,7 +175,7 @@ class ForegroundService : Service(), MethodChannel.MethodCallHandler {
 			addAction(ACTION_NOTIFICATION_PRESSED)
 		}
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-			registerReceiver(broadcastReceiver, intentFilter, Context.RECEIVER_NOT_EXPORTED)
+			registerReceiver(broadcastReceiver, intentFilter, Context.RECEIVER_EXPORTED)
 		} else {
 			registerReceiver(broadcastReceiver, intentFilter)
 		}
